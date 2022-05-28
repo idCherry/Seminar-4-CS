1013f91550bebf8eff8bdadf1f6738b9b830dd05
Margarita <margarita_anis@bk.ru>
2022-05-28 21:53:55 +0300

    Add HOMEWORK Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Цикл принимает на вход два числа (A и B) и возводит число A в натуральную
+// степень B
+
+Console.Write("Введите число A: ");
+double numA = double.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+double numB = double.Parse(Console.ReadLine());
+
+double exponentiation = Math.Pow(numA, numB);
+Console.WriteLine($"{numA} в степени {numB} = {exponentiation}");
+
+
