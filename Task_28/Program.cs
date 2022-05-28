e6c60d8c436d38b9ab814a3bcee94b94b2bb0a73
Margarita <margarita_anis@bk.ru>
2022-05-28 19:15:19 +0300

    Add project Task_28

---
+++ Task_28/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает на вход число N и выдет произведение чисел от 1 до N
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+
+int FindMulNumbers (int number)
+{
+    int mul = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        mul = mul * i; 
+    }
+    return mul;
+}
+int result = FindMulNumbers(num);
+Console.WriteLine($"Произведение чисел от 1 до {num} = {result}");
