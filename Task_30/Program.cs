731b54d99cb246eeeeebf8a72bb5e12132346b3b
Margarita <margarita_anis@bk.ru>
2022-05-28 19:49:37 +0300

    Add project Task_30

---
+++ Task_30/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа выводит массив из 8 элементов, заполненный нулями и единицами в случайном порядке
+
+
+int [] array = new int [8];
+
+void PrintArray (int [] col)
+{
+    int length = col.Length;
+    int index = 0;
+    Console.Write("Массив [");
+    while (index < length)
+    {
+        col[index] = new Random().Next(0, 2);
+        Console.Write($"{col[index]}" + $"  ");
+        index++;
+    }
+    Console.Write("]");
+}
+
+PrintArray(array);
