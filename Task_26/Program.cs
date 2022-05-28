b3eb5736d1349723868f4832f637737a3cf6bd74
Margarita <margarita_anis@bk.ru>
2022-05-28 18:46:38 +0300

    Add project Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа на вход принимает число и выдает кол-во цифр в числе
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+int CountNumbers (int num)
+{
+    int div = 1;
+    int count = 0;
+    while (num > div)
+    {
+        div = div * 10;
+        count++;
+    }
+    return count;
+}
+Console.WriteLine($"Количество цифр в числе = {CountNumbers(number)}");
