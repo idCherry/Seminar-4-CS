1a13cb509d31473eddd4b5a1072abd948e9b281e
Margarita <margarita_anis@bk.ru>
2022-05-28 16:27:48 +0300

    Add project Task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа на вход принимает число (А) и выдает сумму чисел от 1 до А
+
+Console.Write("Введите натуральное число: ");
+int num = int.Parse(Console.ReadLine());
+
+int GetSumMunbers(int number)
+{
+    int sum = 0;
+for (int i = 1; i <= num; i++)
+    {
+        sum = sum  + i;
+    }
+    return sum;
+}
+
+int sumResult = GetSumMunbers(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sumResult}");
