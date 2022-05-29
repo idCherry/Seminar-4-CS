1286ce8177b294e6747467b665ba20e8a1412b3a
Margarita <margarita_anis@bk.ru>
2022-05-29 11:19:11 +0300

    Add HOMEWORK Task_29

---
+++ Task_29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа задаёт массив целых чисел и выводит их на экран.
+// Размер массива определяется пользователем.
+
+Console.Write("Введите длину массива: ");
+int arrayLength = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [arrayLength];
+
+void PrintArray (int [] collection)
+{
+    int length = collection.Length;
+    int i = 0;
+    Console.Write("Массив [");
+    while (i < length)
+    {
+        collection[i] = new Random().Next(0, $"{arrayLength}");
+        Console.Write($"{collection[i]}" + $"  ");
+        i++;
+    }
+    Console.Write("]");
+}
+
+PrintArray(array);
