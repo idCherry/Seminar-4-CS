3b8c5aa8d773a73ffae124d26a34e57739774f5c
Margarita <margarita_anis@bk.ru>
2022-05-29 11:58:11 +0300

    Add HOMEWORK Task_27

---
+++ Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа принимает на вход число и выдаёт сумму цифр в числе
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int GetSumNumbers (int number)
+{
+    int sum = 0;
+   
+    while (num != 0)
+    {
+        sum += num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+int sumResult = GetSumNumbers(num);
+Console.WriteLine($"Сумма цифр в числе = {sumResult}");
